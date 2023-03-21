4f2b7b497362dbee6a25e7ac278b18f230b8e9d0
Wes <wes.hamster@gmail.com>
2023-03-21 13:34:32 -0400

    Add files via upload

---
+++ download_likes.py
@@ -0,0 +1,160 @@
+import browser_cookie3
+import requests
+from urllib.request import urlretrieve
+
+import os
+from pathlib import Path
+
+script_dir = Path(__file__).parent
+output_dir = script_dir / "output"
+cookiejar = browser_cookie3.firefox(domain_name='ifunny.co')
+print(cookiejar)
+
+headers = {
+    "Accept": "application/json",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Cache-Control": "no-cache",
+    "Connection": "keep-alive",
+    "content-type": "application/json",
+    "Host": "ifunny.co",
+    "Pragma": "no-cache",
+    "Referer": "https://ifunny.co/account/smiles",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "same-origin",
+    "Sec-Fetch-Site": "same-origin",
+    "TE": "trailers",
+    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/111.0", #!
+    "x-csrf-token": "8a8473c4934650edfbefd2d2dc61db8c", #!
+    "x-requested-with": "fetch",
+}
+
+
+#https://ifunny.co/api/v1/account/smiles?next=1678975992.919
+
+def load_urls_from_file(filename):
+    try:
+        with open(filename, "r") as file:
+            return file.read().splitlines()
+    except:
+        print(f"File doesn't exist: {filename}")
+        return []
+
+
+def get_items(url):
+    urls = []
+    res = requests.get(url, cookies=cookiejar, headers=headers)
+    print(res)
+    #print(res.json())
+
+    json_res = res.json()
+    next_token = json_res.get("pagination").get("next")
+
+    for item in json_res.get("items"):
+        url = item.get("url")
+        urls.append(url)
+        print(url)
+
+    print(len(json_res.get("items")), "items")
+    print(json_res.get("pagination"))
+    print("next:", next_token)
+
+    return urls, next_token
+
+
+def write_urls_to_file(filename, urls):
+    with open(filename, 'w') as file:
+        file.writelines(
+            url + "\n"
+            for url in urls
+        )
+
+def append_urls_to_file(filename, urls):
+    with open(filename, 'a') as file:
+        file.writelines(
+            url + "\n"
+            for url in urls
+        )
+
+
+def append_url_to_file(filename, url):
+    with open(filename, 'a') as file:
+        file.write(url + "\n")
+
+
+
+
+def main():
+    current_urls_set = set(load_urls_from_file(script_dir / "urls.txt"))
+    next_id = len(current_urls_set) + 1
+
+    print(f"Current urls: {len(current_urls_set)}")
+    print(f"Next ID: {next_id}")
+
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    new_item_urls = []
+
+    base_url = "https://ifunny.co/api/v1/account/smiles?next="
+    next_token=" "
+    next_url = base_url
+
+    while next_token:
+        urls, next_token = get_items(next_url)
+        next_url = base_url + next_token
+
+        for url in urls:
+            if url in current_urls_set:
+                continue
+
+            new_item_urls.append(url)
+
+    print(f"Found {len(new_item_urls)} new items")
+
+    for new_url in reversed(new_item_urls):
+        print(f"Downloading {new_url}")
+        url_filename = new_url.split("/")[-1]
+        dst = output_dir / f"{next_id} {url_filename}"
+        urlretrieve(new_url, dst)
+
+        append_url_to_file("urls.txt", new_url)
+        next_id += 1
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
